a5c1f96b706ec1cdda5191f2f85709e52a8b87c5
梁峰宁 <liangfengning@foxmail.com>
2020-12-02 22:56:47 +0800

    docs: add nav links and faq page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,17 @@ module.exports = {
     title: 'I Show You',
     description: 'The documentation of i-show-you',
     port: '3030',
+    head: [
+        [ 'link', { rel: 'icon', href: '/favicon.ico' } ]
+    ],
+    themeConfig: {
+        logo: '/logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'FAQ', link: '/faq/' },
+            { text: 'API', link: '/api/' },
+            { text: 'Demo', link: 'https://i-show-you.dev.lifeni.life/' },
+        ]
+    },
 }
