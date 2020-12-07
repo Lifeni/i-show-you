8551f78a4ae7046a5bfd62b5811188f3e828e82d
梁峰宁 <liangfengning@foxmail.com>
2020-12-08 00:22:52 +0800

    feat: add backend api and mongo

---
+++ server/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+var (
+	Client *mongo.Client
+)
+
+func ConnectDB() error {
+	var err error
+	Client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	err = Client.Connect(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = Client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Println("Unable to connect to database")
+		return err
+	}
+	log.Println("Connected to database")
+
+	return err
+}
---
+++ server/router/file.go
@@ -0,0 +1,36 @@
+package router
+
+import (
+	"github.com/labstack/echo"
+	"github.com/satori/go.uuid"
+	"net/http"
+)
+
+type File struct {
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Content string `json:"content"`
+	Options struct {
+		Slug string `json:"slug"`
+		Sync string `json:"sync"`
+	}
+}
+
+func CreateFile(c echo.Context) error {
+	f := new(File)
+	if err := c.Bind(f); err != nil {
+		return err
+	}
+
+	var content struct {
+		Message string    `json:"message"`
+		ID      uuid.UUID `json:"id"`
+	}
+
+	id := uuid.NewV4()
+
+	content.Message = "Ok"
+	content.ID = id
+
+	return c.JSON(http.StatusOK, &content)
+}
